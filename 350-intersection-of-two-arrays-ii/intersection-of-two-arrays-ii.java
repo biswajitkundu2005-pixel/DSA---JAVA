aace67b64a9edd6701738843d3479b905f551754
Biswajit Kundu <biswajitkundu2005@gmail.com>
2026-08-03 07:49:43 +0530

    Time: 3 ms (95.22%) | Memory: 45.3 MB (21.53%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Store frequencies of nums1
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // Find common elements
+        for (int num : nums2) {
+            if (map.getOrDefault(num, 0) > 0) {
+                list.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        // Convert ArrayList to int[]
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
+        }
+
+        return ans;
+    }
+}
