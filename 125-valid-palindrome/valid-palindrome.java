7a109494a61e452c7f73c9ddcaded0419ede4247
Biswajit Kundu <biswajitkundu2005@gmail.com>
2026-08-22 23:40:59 +0530

    Time: 15 ms (18.81%) | Memory: 47.3 MB (13.49%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,15 +2,11 @@ class Solution {
     public boolean isPalindrome(String s) {
 
         // 1. Remove non-alphanumeric characters
-        s = s.replaceAll("[^a-zA-Z0-9]", "");
-
-        // 2. Convert to lowercase
-        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         // 3. Reverse
         String rev = new StringBuilder(s).reverse().toString();
 
-        // 4. Compare
         return s.equals(rev);
     }
 }
